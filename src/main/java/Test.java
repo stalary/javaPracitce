ed1acec8ca4c0ec0b63e850dccf91e16ac26e4bc
stalary <452024236@qq.com>
2017-11-25 14:17:24 +0800

    test git

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -14,26 +14,9 @@ public class Test {
 
 
     public static void main(String[] args) throws IOException {
-        Set<Integer> set = new HashSet<>();
-        set.add(1);
-        set.add(1);
-        System.out.println(set);
-    }
-
-
-    private static void insert(int[] a) {
-        int len = a.length;
-        for (int i = 0; i < len; i++) {
-            for (int j = i; j > 0 && a[j] < a[j - 1]; j--) {
-                swap(a, j, j - 1);
-            }
-        }
-    }
-
-    private static void swap(int[] a, int i, int j) {
-        int t = a[i];
-        a[i] = a[j];
-        a[j] = t;
+        int[] arr = new int[]{
+                4, 2, 3
+        };
     }
 
 }
---
+++ src/main/java/com/stalary/algorithm/leetcode/FindAllNumbersDisappearedInAnArray448.java
@@ -0,0 +1,32 @@
+/**
+ * @(#)FindAllNumbersDisappearedInAnArray448.java, 2017-11-25.
+ * <p>
+ * Copyright 2017 Youdao, Inc. All rights reserved.
+ * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.stalary.algorithm.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * FindAllNumbersDisappearedInAnArray448
+ *
+ * @author lirongqian
+ * @since 2017/11/25
+ */
+public class FindAllNumbersDisappearedInAnArray448 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+
+            }
+        }
+    }
+}
