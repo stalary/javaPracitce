25f71dd574f68948325378195d812147382463db
stalary <452024236@qq.com>
2017-12-17 22:41:44 +0800

    2017.12.17 剑指offer done

---
+++ src/main/java/com/stalary/algorithm/swordoffer/Convert.java
@@ -0,0 +1,54 @@
+/**
+ * @(#)Convert.java, 2017-12-17.
+ * <p>
+ * Copyright 2017 Youdao, Inc. All rights reserved.
+ * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.stalary.algorithm.swordoffer;
+
+/**
+ * Convert
+ * <p>
+ * 将二叉搜索树转化为排序好的双向链表
+ *
+ * @author lirongqian
+ * @since 2017/12/17
+ */
+public class Convert {
+
+    /**
+     * 中序遍历二叉树即可实现排序
+     *
+     * 左子树的最后一个结点与根相连，右子树的头结点与根相连
+     * @param
+     * @return
+     */
+    public TreeNode Convert(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+        // 1.将左子树构造成双链表，并返回链表头节点
+        TreeNode left = Convert(root.left);
+        TreeNode p = left;
+        // 2.定位至左子树双链表最后一个节点
+        while (p != null && p.right != null) {
+            p = p.right;
+        }
+        // 3.如果左子树链表不为空的话，将当前root追加到左子树链表
+        if (left != null) {
+            p.right = root;
+            root.left = p;
+        }
+        // 4.将右子树构造成双链表，并返回链表头节点
+        TreeNode right = Convert(root.right);
+        // 5.如果右子树链表不为空的话，将该链表追加到root节点之后
+        if (right != null) {
+            right.left = root;
+            root.right = right;
+        }
+        return left != null ? left : root;
+    }
+}
