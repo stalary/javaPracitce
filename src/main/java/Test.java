dcb3bbce360b1068a6f9d1f62459219ffe785dcb
stalary <452024236@qq.com>
2018-01-02 11:52:57 +0800

    2018.1.2 swordoffer done

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -9,11 +9,24 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) throws Exception {
-        List<Integer> list = new ArrayList<>();
-        list.add(0, 3);
-        list.add(0, 2);
-        list.add(0, 1);
-        System.out.println(list);
+        int[] nums = new int[] {
+                5,2,3,1,9,6,7,13,22,8,20
+        };
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        byte[] bytes = new byte[max + 1];
+        for (int i = 0; i < nums.length; i++) {
+            bytes[nums[i]] = 1;
+        }
+        for (int i = 0; i < bytes.length; i++) {
+            if (bytes[i] == 1) {
+                System.out.print(i + " ");
+            }
+        }
     }
 
 }
--- src/main/java/com/stalary/algorithm/swordoffer/IsContinuous.java
+++ src/main/java/com/stalary/algorithm/swordoffer/IsContinuous.java
@@ -10,6 +10,8 @@ import java.util.Arrays;
 
 /**
  * IsContinuous
+ * <p>
+ * 给定几张牌，判断是否可以作为顺子，0可以代表任意的牌
  *
  * @author lirongqian
  * @since 01/01/2018
@@ -17,102 +19,81 @@ import java.util.Arrays;
 public class IsContinuous {
 
     public static void main(String[] args) {
-        int[] numbers = new int[] {
-                1,0,1,0,0
+        int[] numbers = new int[]{
+                1, 0, 3, 4
         };
         System.out.println(new IsContinuous().isContinuous(numbers));
     }
 
-    public boolean isContinuous(int [] numbers) {
-        if (numbers.length == 0) {
+    public boolean isContinuous(int[] numbers) {
+        if (numbers == null) {
             return false;
         }
-        int min = Integer.MAX_VALUE;
-        int max;
-        int[] temp = new int[numbers.length];
-        System.arraycopy(numbers, 0, temp, 0 , temp.length);
-        Arrays.sort(temp);
-        // 查找最小值
-        for (int i = 0; i < temp.length; i++) {
-            if (temp[i] != 0) {
-                if (temp[i] < min) {
-                    min = temp[i];
-                }
+        // 顺子由五张牌组成
+        if (numbers.length < 5) {
+            return false;
+        }
+        // 排序
+        Arrays.sort(numbers);
+        // 拿到除0以外的最小值
+        int zero = 0;
+        int i = 0;
+        // 统计0的数量
+        for (; i < numbers.length && numbers[i] == 0; i++) {
+            zero++;
+        }
+        // 从非0处开始遍历，若有相同元素则不能组成顺子
+        for (; i < numbers.length - 1 && zero >= 0; i++) {
+            if (numbers[i] == numbers[i + 1]) {
+                return false;
             }
-        }
-        max = temp[temp.length - 1];
-        for (int i = 1; i < temp.length; i++) {
-            if (temp[i] != 0) {
-                if (temp[i] == temp[i -1]) {
-                    return false;
-                }
-            }
-        }
-        if (max == min) {
-            return true;
-        }
-        int[] sort = new int[max - min + 1];
-        int index = 0;
-        for (int i = min; i <= max; i++) {
-            sort[index++] = i;
-        }
-        if (Arrays.equals(sort, temp)) {
-            return true;
-        } else {
-            if (temp[0] != 0) {
+            // 如果当前元素加1加上剩余0的数量大于下一个元素，则说明可以到达下一个元素
+            if (numbers[i] + 1 + zero >= numbers[i + 1]) {
+                // 减去使用的0
+                zero -= numbers[i + 1] - numbers[i] - 1;
+            } else {
                 return false;
             }
         }
-        int j;
-        if (sort.length < temp.length) {
-            return true;
-        }
-        for (j = 0; j < temp.length; j++) {
-            if (temp[j] == sort[0]) {
-                break;
-            }
-        }
-        int num = 0;
-        for (int i = 0; i < temp.length; i++) {
-                if (temp[j] != sort[i]) {
-                    num++;
-                } else {
-                    j++;
-                }
-        }
-        for (int i = 0; i < temp.length; i++) {
-            if (temp[i] == 0) {
-                num--;
-            }
-        }
-        return num == 0;
+        // 如果i可以到达末尾，则说明可以组成顺子
+        return i == numbers.length - 1;
     }
 
-    public boolean isContinuous1(int [] numbers) {
+    public boolean isContinuous1(int[] numbers) {
+        // 顺子由五张牌组成
         if (numbers.length != 5) {
             return false;
         }
+        // 顺子最大13，最小1，特殊牌为0
         int min = 14;
         int max = -1;
         int flag = 0;
         for (int i = 0; i < numbers.length; i++) {
             int number = numbers[i];
+            // 如果存在非法的值则跳出
             if (number < 0 || number > 13) {
                 return false;
             }
+            // 当前数字为特殊值，则继续
             if (number == 0) {
                 continue;
             }
-            if (((flag >> number) & 1) == 1) {
+            // 如果有非0的重复数字则无法组成顺子
+            // 将1右移numbers[i]位，然后与flag进行按位与运算
+            // 若flag上的第numbers[i]已为1，说明该位已被占用，数字重复了，运算的结果大于0,返回false
+            if (((1 << number) & flag) > 0) {
                 return false;
             }
+            // 通过“或操作”填充flag中不同的二进制位
             flag |= (1 << number);
+            // 找出最大值与最小值
             if (number > max) {
                 max = number;
             }
             if (number < min) {
                 min = number;
             }
+            // 判断最大值与最小值的差是否小于5
             if (max - min >= 5) {
                 return false;
             }
