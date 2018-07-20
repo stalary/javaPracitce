5b366f9dac968a160bffa05923acf2d9a3263d59
stalary <452024236@qq.com>
2018-07-20 09:35:18 +0800

    MOD:

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,11 +1,7 @@
+import java.util.stream.IntStream;
+
 public class Test {
     public static void main(String[] args) {
-        Thread.currentThread().interrupt();
-        System.out.println(Thread.currentThread().isInterrupted());
-        boolean interrupted = Thread.interrupted();
-        System.out.println(interrupted);
-        System.out.println(Thread.currentThread().isInterrupted());
-        Thread.currentThread().interrupt();
-        System.out.println(Thread.currentThread().isInterrupted());
+        IntStream.rangeClosed(0, 10).forEach(System.out::println);
     }
 }
