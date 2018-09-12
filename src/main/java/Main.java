e11db4a8f37c158e218269b3de6e2099a9a23491
stalary <452024236@qq.com>
2018-09-12 14:09:02 +0800

    MOD

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,22 +12,20 @@
  */
 public class Main {
 
-    public static void main(String[] args) throws ClassNotFoundException {
-        Class clazz = Class.forName("java.util.Random");
-        System.out.println(clazz);
+    public static void main(String[] args) {
+        String str = "12346";
+        System.out.println(parseInt(str));
     }
 
-    public static class Child extends Parent {
-        static {
-            System.out.println("child static");
+    public static int parseInt(String num) {
+        char[] chars = num.toCharArray();
+        int index = 1;
+        int sum = 0;
+        for (int i = chars.length - 1; i >= 0; i--) {
+            sum += index * (chars[i] - '0');
+            index *= 10;
         }
-    }
-
-    public static class Parent {
-        public static final String name = "stalary";
-        static {
-            System.out.println("parent static");
-        }
+        return sum;
     }
 
 }
