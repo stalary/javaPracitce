4a2138c3fbed04cfe81e7d321c2f8f23d67b371e
stalary <452024236@qq.com>
2018-10-31 10:08:26 +0800

    MOD

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,12 @@
-import java.util.Stack;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(1);
-        stack.push(4);
-        stack.push(2);
-        stack.push(3);
-        stack.push(6);
-
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            System.out.println(in.nextInt() + in.nextInt());
+        }
     }
 
 }
